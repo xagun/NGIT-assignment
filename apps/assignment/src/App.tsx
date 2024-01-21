38b784a70767fb0a9df1e098c84cd05846662d0a
xagun <xagun.shrestha@gmail.com>
2024-01-21 16:45:52 +0545

    Modal assignment ui used in the vite app

--- apps/assignment/src/App.tsx
+++ apps/assignment/src/App.tsx
@@ -1,158 +1,92 @@
-// import { useState } from 'react'
-
-// import './App.css'
-// // import "assignment-ui/styles.css"
-// // import { add } from "assignment-ui"
-// // import { subtract } from "assignment-ui"
-// import { Modal } from "assignment-ui"
-
-// import { Button } from "@/components/ui/button"
-
-// import { Skeleton } from "@/components/ui/skeleton";
-
-
-// function App() {
-//   // const [count, setCount] = useState(0)
-//   const userData = [{name:"Testman", dob:"1998-09-22", description:"hello"},
-//    {name:"Testman 1", dob:"1998-09-22", description:"hello"},
-//    {name:"Testman 2", dob:"1998-09-22", description:"hello"},
-//    {name:"Testman 3", dob:"1998-09-22", description:"hello"},
-//    {name:"Testman 5", dob:"1998-09-22", description:"hello"},
-//    {name:"Testman", dob:"1998-09-22", description:"hello"}]
-
-//   return (
-
-    
-//     <>
-
-
-
-// {/* <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-
-//   <div className="fixed inset-0 bg-gray-500 bg-blue-800 transition-opacity"></div>
-
-//   <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-//     <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-  
-//       <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-//         <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-//           <div className="">
-//           <div>
-//       <Skeleton className="w-[300px] h-[25px] rounded-full m-2"/>
-//       <Skeleton className="w-[300px] h-[25px] rounded-full m-2"/>
-//       <Skeleton className="w-[300px] h-[25px] rounded-full m-2"/>
-//       <Skeleton className="w-[300px] h-[25px] rounded-full m-2"/>
-//       <Skeleton className="w-[300px] h-[25px] rounded-full m-2"/>
-//     </div>
-//             <Modal userData={userData}></Modal>
-//               <Button className='p-6 bg-yellow-500'>View All</Button>
-//           </div>
-//         </div>
-    
-//       </div>
-//     </div>
-//   </div>
-// </div> */}
-// <Modal userData={userData}></Modal>
-
-
-
-
-      
-//     </>
-//   )
-// }
-
-// export default App
-
-
-
 "use client";
 // src/App.tsx
-import React, { useState, useEffect } from 'react';
-import { Modal } from 'assignment-ui';
-import { Button } from "@/components/ui/button"
-
+import React, { useState, useEffect } from "react";
+import { Modal } from "assignment-ui";
 import { Skeleton } from "@/components/ui/skeleton";
-
 
 const App: React.FC = () => {
   const [data, setData] = useState<any[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
+  const fetchData = async () => {
+    try {
+      setTimeout(async () => {
+        const response = await fetch("https://dummyjson.com/users");
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const result = await response.json();
+        setIsLoading(false);
+        setData(result.users.slice(0, 5));
+      }, 3000);
+    } catch (error) {
+      setError("Error fetching data");
+      console.error("Error fetching data:", error);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-
-        // Simulating a delay of 5 seconds
-        setTimeout(async () => {
-          const response = await fetch('https://dummyjson.com/users');
-
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-
-          const result = await response.json();
-        setIsLoading(false);
-
-          setData(result.users.slice(0,5));
-        }, 3000); // 5000 milliseconds = 5 seconds
-      } catch (error) {
-        setError('Error fetching data');
-        console.error('Error fetching data:', error);
-      } 
-    };
-
     fetchData();
   }, []);
 
   return (
     <div>
+      <Modal>
+        {isLoading ? (
+          <>
+            <Skeleton className="h-6 w-[180px] m-2" />
 
-  
-            <Modal>
-      <>
-      
-      {isLoading ?
-      
-    <>
- 
-          {
-            Array(5)
-            .fill(0)
-            .map((_, i) => (
-              <div key={i} className="flex items-center space-x-4 m-4">
-                <Skeleton className="h-12 w-12 rounded-full" />
-                <div className="space-y-2">
-                  <Skeleton className="h-4 w-[200px]" /> 
-                  <Skeleton className="h-4 w-[100px]" />
+            {Array(5)
+              .fill(0)
+              .map((_, i) => (
+                <div key={i} className="flex items-center space-x-2 my-4">
+                  <Skeleton className="h-10 w-10 rounded-full" />
+                  <div className="space-y-2">
+                    <Skeleton className="h-4 w-[150px]" />
+                    <Skeleton className="h-4 w-[80px]" />
+                  </div>
                 </div>
-              </div>
-            ))
-          }
-    </>
-    :<ul>
-    {data.map((item: { id: string; firstName: string; lastName: string; age:number;image:string}) => (
-       <div key = {item.id} className="flex items-center space-x-4 m-4">
-       <img src = {item.image} className="h-12 w-12 rounded-full" />
-       <div className="space-y-2">
-         <p className="h-4 w-[200px]">{item.firstName} {" "} {item.lastName} </p>
-         <p className="h-4 w-[100px]">{item.age} years</p>
-       </div>
-     </div>
-         
-          ))}
-    </ul>
-      }
+              ))}
+          </>
+        ) : (
+          <>
+            <h1 className="text-lg m-2">24 Birthdays Today</h1>
 
-
-
-
-</>
+            <ul>
+              {data.map(
+                (item: {
+                  id: string;
+                  firstName: string;
+                  lastName: string;
+                  age: number;
+                  image: string;
+                }) => (
+                  <div
+                    key={item.id}
+                    className="flex items-center space-x-2 my-4"
+                  >
+                    <img src={item.image} className="h-10 w-10 rounded-full" />
+                    <div className="space-y-2">
+                      <p className="h-3 w-[200px] text-sm">
+                        {item.firstName} {item.lastName}{" "}
+                      </p>
+                      <p className="h-3 w-[100px] opacity-70 text-xs">
+                        {item.age} years
+                      </p>
+                    </div>
+                  </div>
+                )
+              )}
+            </ul>
+            <div className="flex justify-center">
+              <button className="bg-gradient-to-r from-[#e171cd] to-[#ff69a4] text-white px-4 py-2 my-4 rounded text-white w-[280px] h-[50px] shadow-[0px_5px_8px_0px_rgba(251,152,202,255)]">
+                View All
+              </button>
+            </div>
+          </>
+        )}
       </Modal>
-
     </div>
   );
 };
--- packages/assignment-ui/src/components/Modal.tsx
+++ packages/assignment-ui/src/components/Modal.tsx
@@ -1,126 +1,32 @@
-// "use client";
-
-// import React from "react";
-// import { ReactNode } from "react";
-
-// interface UserData {
-//     name: string;
-//     dob: string;
-//     description: string;
-//    }
-   
-
-// interface ButtonProps {
-//   userData:UserData[]
-// }
-
-// export const Modal = ({  userData }: ButtonProps) => {
-//   return (
-//     // <button
-//     //   className={className || ''}
-//     //   onClick={() => alert(`Hello from your ${appName} app!`)}
-//     // >
-//     //   {children}
-//     // </button>
-//     <>
-
-// <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-
-// <div className="fixed inset-0 bg-[#ef89b1]  transition-opacity"></div>
-
-// <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-//   <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-
-//     <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-[30px_30px_90px_10px_rgba(0,0,0,0.3)] transition-all sm:my-8 sm:w-[400px] sm:max-w-lg">
-//       <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-//         <div className="">
-//         {/* <div>
-//     <Skeleton className="w-[300px] h-[25px] rounded-full m-2"/>
-//     <Skeleton className="w-[300px] h-[25px] rounded-full m-2"/>
-//     <Skeleton className="w-[300px] h-[25px] rounded-full m-2"/>
-//     <Skeleton className="w-[300px] h-[25px] rounded-full m-2"/>
-//     <Skeleton className="w-[300px] h-[25px] rounded-full m-2"/>
-//   </div> */}
-//     {userData.map((ud)=>(
-//         <p className="p-4 bg-red-600">
-// {ud.name}
-//         </p>
-//     ))}
-
-//   {/* {userData.map((ud)=>(
-//       <p className="p-4">
-// {ud.name}
-//       </p>
-//   ))} */}
-  
-//   <div className="flex justify-center">
-//   <button className='p-2 bg-yellow-400'>View All</button>
-// </div>
-//         </div>
-//       </div>
-  
-//     </div>
-//   </div>
-// </div>
-// </div>
-  
-    
-
-//     </>
-//   );
-// };
-
-
 "use client";
 
 import React from "react";
 import { ReactNode } from "react";
 
-
-
 interface ModalProps {
   children: ReactNode;
- 
 }
 
 export const Modal: React.FC<ModalProps> = ({ children }) => {
-
   return (
- 
     <>
-
-<div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-
-  <div className="fixed inset-0 bg-[#ef89b1]  transition-opacity"></div>
-
-  <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-    <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-  
-      <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-[30px_30px_90px_10px_rgba(0,0,0,0.3)] transition-all sm:my-8 sm:w-[360px] sm:max-w-lg">
-        <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-          <div className="">
-          <h1 className="text-center text-xl font-bold">
-  24 Birthdays Today
-</h1>
-
-    {children}
-
-
-    
-    <div className="flex justify-center">
-    <button
-  className="bg-gradient-to-r from-pink-400 to-purple-400 text-white px-4 py-2 my-4 rounded text-white w-[250px] h-[50px]"  
->
-  View All
-</button>
-</div>
+      <div
+        className="relative z-10"
+        aria-labelledby="modal-title"
+        role="dialog"
+        aria-modal="true"
+      >
+        <div className="fixed inset-0 bg-[#ef89b1]  transition-opacity"></div>
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-[80px_60px_40px_0px_rgba(223,110,154,255)] transition-all sm:my-8 sm:w-[320px] sm:h-[450px] sm:max-w-lg">
+              <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                {children}
+              </div>
+            </div>
           </div>
         </div>
-    
       </div>
-    </div>
-  </div>
-</div>
     </>
   );
 };
